152a16087440d37af47a75a0b48ca19423f8b584
Abhi solanki <abhi85076@gmail.com>
2025-12-10 19:43:49 +0530

    fixed the padding issue in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between flex-row w-full bg-[#FFCC66] w-[calc[100%-80vh] px-20 py-4 items-center'>
+    <div className='flex justify-between flex-row w-full bg-[#FFCC66] w-[calc[100%-78vh] px-20 py-[13px] items-center'>
         <div className='flex gap-2 items-center'>
         <p className='text-[#3E8E7E] text-shadow-xs text-shadow-[#3E8E7E] font-medium text-lg'>Created by Abhi Solanki with  </p>
         <img style={{width:20,height:20}} src="/icons/heart.png" alt="heart-png" className='drop-shadow-red-500 drop-shadow-xs' /> 
